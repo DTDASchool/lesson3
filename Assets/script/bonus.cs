9a9d764e2f2fb18e36dae90db9f17eaf02140281
DTDA Games <51952304+DTDASchool@users.noreply.github.com>
2019-07-14 15:32:02 +0200

    Add files via upload

---
+++ Assets/script/bonus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bonus : MonoBehaviour
+{
+	public GameObject manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+       manager = GameObject.Find ("manager");	 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter (Collider other)
+	{ 
+		if(other.gameObject.tag == "Player")
+		{
+			manager.GetComponent<manager>().score +=  1;
+			Debug.Log("touché");
+		}
+	}
+}
---
+++ Assets/script/manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manager : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject spawn;
+	public int score;
+	public int time = 20;
+	public float tempTime = 0f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(player, spawn.transform.position, Quaternion.identity);
+		score= 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(tempTime < 1f)
+		{
+			tempTime += Time.deltaTime;
+		}
+		else 
+		{
+			time -= 1;
+			tempTime = 0f;
+		}
+		
+		displayScore();
+    }
+	
+	public void displayScore()
+	{
+		GameObject.Find("score").GetComponent<Text>().text = score.ToString();
+	}
+}
